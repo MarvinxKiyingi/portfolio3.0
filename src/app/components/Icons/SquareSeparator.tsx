e199df26728ffd6dc065499185589d0a4e1f8925
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2023-11-11 20:13:50 +0100

    feat(app): erase default styling & initiate my own (#1) (#2)

---
+++ src/app/components/Icons/SquareSeparator.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SquareSeparator = () => {
+  return (
+    <svg
+      width='4'
+      height='5'
+      viewBox='0 0 4 5'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect y='0.5' width='4' height='4' fill='#181717' />
+      <rect y='0.5' width='4' height='4' fill='#181717' fill-opacity='0.2' />
+      <rect y='0.5' width='4' height='4' fill='#181717' fill-opacity='0.2' />
+      <rect y='0.5' width='4' height='4' fill='#181717' fill-opacity='0.2' />
+      <rect y='0.5' width='4' height='4' fill='#181717' fill-opacity='0.2' />
+      <rect y='0.5' width='4' height='4' fill='#181717' fill-opacity='0.2' />
+    </svg>
+  );
+};
+
+export default SquareSeparator;
