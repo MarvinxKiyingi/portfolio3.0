d66f5dc753dc9ca58c49b58c3f9852630f753a73
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2024-01-02 15:12:27 +0100

    fix(NavPill): use js for pathname check

--- src/app/components/NavPill/NavPill.tsx
+++ src/app/components/NavPill/NavPill.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import styles from './NavPill.module.scss';
 import Button from '../Button/Button';
-import { usePathname } from 'next/navigation';
 
 const NavPill = () => {
-  const pathname = usePathname();
+  const pathname = window.location.pathname;
+  console.log('pathname:', pathname);
   const navigateTo = pathname === '/' ? '#Work' : pathname;
   const navItems = [{ to: navigateTo, text: 'Work' }];
 
