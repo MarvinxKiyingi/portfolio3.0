f61699ad38b4f8e30c3e870cf093c2c73edf2fa0
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2024-01-02 15:36:40 +0100

    fix(NavPill): switch back to use usePathname

--- src/app/components/NavPill/NavPill.tsx
+++ src/app/components/NavPill/NavPill.tsx
@@ -1,12 +1,12 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavPill.module.scss';
 import Button from '../Button/Button';
+import { usePathname } from 'next/navigation';
 
 const NavPill = () => {
-  const pathname = window.location.pathname;
-  console.log('pathname:', pathname);
-  const navigateTo = pathname === '/' ? '#Work' : pathname;
+  const pathname = usePathname();
+  const navigateTo = pathname === '/' ? '#Work' : '/';
   const navItems = [{ to: navigateTo, text: 'Work' }];
 
   return (
