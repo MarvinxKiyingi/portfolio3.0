afb61edb072bb6b78b748097582e5f4a291e848c
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2024-04-22 22:12:25 +0200

    feat(name): style & position name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,23 @@ import styles from './page.module.scss';
 import { client } from './utils/sanity/client';
 import { PortableText } from '@portabletext/react';
 import { IHome } from './home';
+import Link from 'next/link';
 
 export default async function Home() {
   const home = await client.fetch<IHome[]>(`*[name == "Home"]`);
   const { blockList } = home[0];
   const descriptionRichTextBlocks = blockList[0].richTextEditor;
 
-  return <main></main>;
+  return (
+    <main className={styles.wrapper}>
+      <div className={styles.gridItem}>
+        <h1 className={styles.name}>
+          <Link href='/' className={styles.initialsWrapper}>
+            Marvin Kiyingi
+          </Link>
+        </h1>
+      </div>
+      <div className={styles.gridItem}></div>
+    </main>
+  );
 }
