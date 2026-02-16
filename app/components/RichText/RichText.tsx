6a33fd529220e92e0abcedf6b945123e67ac73ab
Marvin Kiyingi <72855673+mrvlab@users.noreply.github.com>
2026-02-16 08:15:12 +0100

    refactor(RichText): enhance Tailwind CSS classes for improved spacing and presentation

--- app/components/RichText/RichText.tsx
+++ app/components/RichText/RichText.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const RichText = ({ content }: { content: SanityRichText }) => {
   if (!content) return null;
   return (
-    <div className="prose max-w-none text-inherit">
+    <div className="prose max-w-none text-inherit prose-p:my-3 prose-p:first:mt-0 prose-p:last:mb-0">
       <PortableText value={content} />
     </div>
   );
